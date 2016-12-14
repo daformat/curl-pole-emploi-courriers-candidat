6b5018c5dfb20472863bf365407c782ed0a20c17
daformat <mathieu@digitalcuisine.fr>
2016-12-14 17:13:35 +0100

    Cleanup, comment, and add notification triggering and iMessage sending when on macOS (notification is automatic, iMessage is sent only if the --imsg option is passed with the correct number/email to send an iMessage to)

--- pe-curl.sh
+++ pe-curl.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env zsh
 
-zparseopts -A ARGUMENTS -id: -pass: -zip:
+zparseopts -A ARGUMENTS -id: -pass: -zip: -imsg:
 
 identifiant=$ARGUMENTS[--id]
 password=$ARGUMENTS[--pass]
 zipcode=$ARGUMENTS[--zip]
+imessage_address=$ARGUMENTS[--imsg]
 
 autoload colors
 if [[ "$terminfo[colors]" -gt 8 ]]; then
@@ -46,7 +47,7 @@ formaction=`echo $auth_page2 | grep -oE "action=\"(.*)\" method=\"post\" id=\"fo
 echo "Authentification..."
 home_candidat=`curl -s -k -L "https://candidat.pole-emploi.fr$formaction" --data-urlencode "t%3Aformdata=$formdata" --data "t%3Asubmit=%5B%22boutonValider%22%2C%22boutonValider%22%5D&champMotDePasse=$password&champTexteCodePostal=$zipcode&boutonValider=Se+connecter" --cookie cookies.txt --cookie-jar cookies.txt`
 
-echo "Récupération des courriers..."
+echo "Récupération de la liste des courriers..."
 
 # we first get a page that is a javascript submited form, so we need to submit the form on our own, this adds a new cookie with the courrier session ID
 mes_courriers=`curl -s -k -L "https://candidat.pole-emploi.fr/candidat/espacepersonnel/regroupements/mesechangesavecpoleemploi.mes_courriers:debrancherversleservice" --cookie cookies.txt`
@@ -59,13 +60,18 @@ courriers=`curl -s -k -L "$action" --data-urlencode "jeton=$jeton" --cookie cook
 # extract mail links (first filtering table.listingPyjama only thanks to hxselect)
 liens_courriers=`echo $courriers | hxselect table.listingPyjama | hxwls -b "$action"`
 
+# CASE 0: No link was found
+# -------------------------
 # If we found no links at all, we just state that and return
 if [ -z $liens_courriers ]; then
   print -P "%BAucun courrier n'a été trouvé%b"
   return
 fi
 
+# -------------------------
+
 # Loop over our links
+nouveaux_courriers_telecharges=0
 printf '%s\n' "$liens_courriers" | while IFS= read -r lien
 do
   #echo "${GREEN}[Courrier trouvé]${RESET} $lien"
@@ -81,7 +87,7 @@ do
   else
 
     # If no pdf was found in the current directory, let's download it
-    echo "${BOLD_GREEN}[Nouveau courrier]${RESET} Courrier n°$num_pdf..."
+    echo "${BOLD_GREEN}[Nouveau courrier]${RESET} Téléchargement du courrier n°$num_pdf..."
     # this should NOT BE NEEDED since it's just a wrapper for displaying the pdf file in an iframe when you are using a browser
     # HOWEVER commenting the request will trigger a 500 error on the server... so yeah, we'll just keep that extra request
     page_courrier=`curl -s -k -L "$lien" --cookie cookies.txt`
@@ -91,9 +97,77 @@ do
 
     # Grab the pdf and save it to the current directory
     fichier_pdf=`curl -k -L --progress-bar -o "$num_pdf.pdf" "https://courriers.pole-emploi.fr/courriersweb/affichagepdf:pdf/$num_pdf" --cookie cookies.txt`
+
+    # finally set the flag so we know at least one pdf was found.
+    let 'nouveaux_courriers_telecharges++'
   fi
 
 done
+
+# Ok, now that we're done, let's provide the user some feedback on what was done
+if [ $nouveaux_courriers_telecharges -gt 0 ]
+then
+
+  # CASE 1: we did find new PDFs to download
+  # ----------------------------------------
+
+  # Check how many and pluralize accordingly
+  if [ $nouveaux_courriers_telecharges -eq 1 ]
+	then
+    texte="nouveau courrier téléchargé"
+	else
+		texte="nouveaux courriers téléchargés"
+	fi
+
+  # Store & print output
+  output="%B$nouveaux_courriers_telecharges $texte%b"
+  print -P $output
+
+  # If on macOS, we can have fun using osascript
+  if hash osascript 2>/dev/null;
+  then
+    # Trigger a notification
+    osascript -e "display notification \"$nouveaux_courriers_telecharges $texte\" with title \"Pôle emploi\" sound name \"Pop\""
+    if [ ! -z $imessage_address ]
+    then
+      # We can then send an iMessage...
+      osascript<<END
+  on is_running(appName)
+    tell application "System Events" to (name of processes) contains appName
+  end is_running
+
+  set was_running to is_running("Messages")
+
+  tell application "Messages"
+    set targetService to 1st service whose service type = iMessage
+    set targetBuddy to buddy "$imessage_address" of targetService
+    send "Pôle emploi : $nouveaux_courriers_telecharges $texte" to targetBuddy
+  end tell
+
+  if was_running is not true then
+    quit app "Messages"
+  end if
+END
+    fi
+  fi
+
+  # Heck let's even SAY it if the computer can !
+  if hash say 2>/dev/null;
+  then
+      say "Alerte pôle-emploi !"
+      say "$nouveaux_courriers_telecharges $texte"
+  fi
+
+else
+
+  # CASE 2: we correctly got a PDF list, but no new PDF was found
+  # -------------------------------------------------------------
+
+  # Store & print output
+  output="%BAucun nouveau courrier n'a été trouvé%b"
+  print -P $output
+fi
+
 
 # Delete cookies ?! http://img12.deviantart.net/d497/i/2015/287/8/5/cookie_monster_____by_supergecko99-d9d2smd.png
 rm cookies.txt
